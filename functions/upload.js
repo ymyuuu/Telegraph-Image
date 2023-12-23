7b4cf29acb3179ee60a23bba6b4237a3a87c2a79
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2023-12-24 02:11:28 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,32 +1,19 @@
-export async function onRequestPost(context) {
+export async function onRequestPost(context) {  // Contents of context object  
     const {   
-        request,
-        env,
-        params,
-        waitUntil,
-        next,
-        data,
-    } = context;
-
-    const url = new URL(request.url);
-
-    // 检查路径是否为 admin、file 或者根路径
-    if (url.pathname.startsWith('/admin') || url.pathname.startsWith('/file') || url.pathname === '/') {
-        // 如果是 admin、file 或者根路径，则执行请求转发
-        const response = await fetch('https://telegra.ph' + url.pathname + url.search, {
-            method: request.method,
-            headers: request.headers,
-            body: request.body,
-        });
-
-        return response;
-    } else {
-        // 如果不是 admin、file 或者根路径，则返回 "你好"
-        return new Response("你好", {
-            status: 200,
-            headers: {
-                "Content-Type": "text/plain",
-            },
-        });
-    }
-}
+        request, // same as existing Worker API    
+    env, // same as existing Worker API    
+    params, // if filename includes [id] or [[path]]   
+     waitUntil, // same as ctx.waitUntil in existing Worker API    
+     next, // used for middleware or to fetch assets    
+     data, // arbitrary space for passing data between middlewares 
+     } = context;
+     context.request
+     const url = new URL(request.url);
+     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+         method: request.method,
+         headers: request.headers,
+         body: request.body,
+     });
+    return response;
+  }
+  
